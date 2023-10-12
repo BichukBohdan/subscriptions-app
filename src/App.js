e3f134a168f705baf2a0fdd197c29770659d49e0
BichukBohdan <bichuk1234.kk@gmail.com>
2023-10-12 18:20:17 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  HomePage,
+} from './pages';
+import './App.css';
+
+import {useStateContext} from './contexts/ContextProvider';
+import DashboardComponent from "./components/DashboardComponent";
+
+
+const App = () => {
+  const {
+    isLogged
+  } = useStateContext();
+
+
+  return (
+      <>
+        {isLogged ? (
+            <DashboardComponent/>
+        ) : (
+            <HomePage/>
+        )}
+      </>
+  );
+};
+
+export default App;
